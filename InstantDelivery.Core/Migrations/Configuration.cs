1ba6296e162ae762aec0254559622bc46a0ca84c
Marcin Chudy <marcinchudy94@gmail.com>
2015-10-21 21:27:00 +0200

    Enable automatic migrations

--- InstantDelivery.Core/Migrations/Configuration.cs
+++ InstantDelivery.Core/Migrations/Configuration.cs
@@ -1,15 +1,12 @@
 namespace InstantDelivery.Core.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<InstantDelivery.Core.InstantDeliveryContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "InstantDelivery.Core.InstantDeliveryContext";
         }
 
