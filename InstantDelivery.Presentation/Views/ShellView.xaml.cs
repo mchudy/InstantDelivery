765a111762a3dd63b373045a5329a51b81ab6ec0
Tygrysku <duszakp@student.mini.pw.edu.pl>
2015-10-26 01:52:44 +0100

    Installing MaHap.Metro templates and styles

--- InstantDelivery.Presentation/Views/ShellView.xaml.cs
+++ InstantDelivery.Presentation/Views/ShellView.xaml.cs
@@ -11,13 +11,14 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace InstantDelivery.Views
 {
     /// <summary>
     /// Interaction logic for ShellView.xaml
     /// </summary>
-    public partial class ShellView : Window
+    public partial class ShellView
     {
         public ShellView()
         {
