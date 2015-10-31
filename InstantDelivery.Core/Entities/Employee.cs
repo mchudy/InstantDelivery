e46675cfb50eea9fa0c853004f9323b8a488376f
Tygrysku <duszakp@student.mini.pw.edu.pl>
2015-10-31 16:05:10 +0100

    reverting changes, keeping columns

--- InstantDelivery.Core/Entities/Employee.cs
+++ InstantDelivery.Core/Entities/Employee.cs
@@ -12,27 +12,32 @@ namespace InstantDelivery.Core.Entities
         public int EmployeeId { get; set; }
 
         [Required(ErrorMessage = "To pole jest wymagane")]
-        [RegularExpression("[A-ZĄĆĘŁŃÓŚŹŻ]{1}[a-ząćęłńóśżź]+", ErrorMessage = "Proszę podać poprawne imię")]
+        [RegularExpression("[A-Z]{1}[a-z]+", ErrorMessage = "Proszę podać poprawne imię")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "To pole jest wymagane")]
-        [RegularExpression("[A-ZĄĆĘŁŃÓŚŹŻ]{1}[a-ząćęłńóśżź]+", ErrorMessage = "Proszę podać poprawne nazwisko")]
+        [RegularExpression("[A-Z]{1}[a-z]+", ErrorMessage = "Proszę podać poprawne nazwisko")]
         public string LastName { get; set; }
 
         public Gender Gender { get; set; }
         public DateTime? DateOfBirth { get; set; }
-        public string PlaceOfBirth { get; set; }
 
-        [RegularExpression("\\d{9}", ErrorMessage = "Proszę podać poprawny numer telefonu")]
+        [Phone(ErrorMessage = "Proszę podać poprawny numer telefonu")]
         public string PhoneNumber { get; set; }
 
         public string Email { get; set; }
         public string Pesel { get; set; }
+        public string PlaceOfBirth { get; set; }
+        public string Comments { get; set; }
+        public string FamilyName { get; set; }
+        public string MotherMaidenName { get; set; }
+        public string MotherName { get; set; }
+        public string FatherName { get; set; }
 
         [Range(0.0, int.MaxValue)]
         public decimal Salary { get; set; }
         public DateTime? HireDate { get; set; }
-
+        public string Citizenship { get; set; }
         public virtual ICollection<Package> Packages { get; set; }
     }
 }
--- InstantDelivery.Core/InstantDeliveryContext.cs
+++ InstantDelivery.Core/InstantDeliveryContext.cs
@@ -9,5 +9,27 @@ namespace InstantDelivery.Core
         public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<Package> Packages { get; set; }
         public DbSet<VehicleModel> VehicleModels { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            BuildAddressModel(modelBuilder);
+        }
+
+        private static void BuildAddressModel(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Types<Package>()
+                .Configure(ctc => ctc.Property(cust => cust.ShippingAddress.Street).HasColumnName("ShipAddressStreet"));
+            modelBuilder.Types<Package>()
+                .Configure(ctc => ctc.Property(cust => cust.ShippingAddress.City).HasColumnName("ShipAddressCity"));
+            modelBuilder.Types<Package>()
+                .Configure(ctc => ctc.Property(cust => cust.ShippingAddress.Country).HasColumnName("ShipAddressCountry"));
+            modelBuilder.Types<Package>()
+                .Configure(ctc => ctc.Property(cust => cust.ShippingAddress.Number).HasColumnName("ShipAddressNumber"));
+            modelBuilder.Types<Package>()
+                .Configure(ctc => ctc.Property(cust => cust.ShippingAddress.PostalCode).HasColumnName("ShipAddressPostalCode"));
+            modelBuilder.Types<Package>()
+                .Configure(ctc => ctc.Property(cust => cust.ShippingAddress.State).HasColumnName("ShipAddressState"));
+        }
     }
 }
