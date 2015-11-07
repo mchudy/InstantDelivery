3a8792483426dedb1d78de80f22e057f2b27c11e
Tygrysku <duszakp@student.mini.pw.edu.pl>
2015-11-07 11:43:25 +0100

    Some creepy charts

---
+++ InstantDelivery.ViewModel/ViewModels/StatisticsViewModels/StatisticsDeliveredPackagesViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Caliburn.Micro;
+using InstantDelivery.Services;
+using PropertyChanged;
+
+namespace InstantDelivery.ViewModel
+{
+    public class StatisticsDeliveredPackagesViewModel : Screen
+    {
+        public StatisticsService service;
+        public ObservableCollection<Population> Budget { get; } = new ObservableCollection<Population>();
+
+        public StatisticsDeliveredPackagesViewModel(StatisticsService service)
+        {
+            this.service = service;
+
+            var valueOfPackages = service.ValueOfAllPackages();
+            var employeesSalaries = service.EmployeesSalaries();
+            var taxes = service.Taxes(valueOfPackages, employeesSalaries);
+            Budget.Add(new Population() { Name = "Wartość dostarczanych paczek", Count = valueOfPackages });
+            Budget.Add(new Population() { Name = "Pensje pracowników", Count = employeesSalaries });
+            Budget.Add(new Population() { Name = "Podatki", Count = taxes });
+        }
+    }
+
+    [ImplementPropertyChanged]
+    public class Population
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int Count { get; set; } = 0;
+    }
+}
---
+++ InstantDelivery.ViewModel/ViewModels/StatisticsViewModels/StatisticsEmployeesVehiclesViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using Caliburn.Micro;
+using InstantDelivery.Services;
+
+namespace InstantDelivery.ViewModel
+{
+    public class StatisticsEmployeesVehiclesViewModel : Screen
+    {
+        public StatisticsService service;
+        public ObservableCollection<Population> Values { get; } = new ObservableCollection<Population>();
+
+        public StatisticsEmployeesVehiclesViewModel(StatisticsService service)
+        {
+            this.service = service;
+
+            var numberOfEmployees = service.NumberOfEmployees();
+            var numberOfVehicles = service.NumberOfVehicles();
+            var numberOfAllPackages = service.NumberOfAllPackages();
+            var numberOfPackagesWithEmployee = service.NumberOfPackagesWithEmployee();
+            var numberOfPackagesWithoutEmployee = service.NumberOfPackagesWithoutEmployee();
+            var numberOfUsedVehicles = service.NumberOfUsedVehicles();
+            var numberOfUnusedVehicles = service.NumberOfUnusedVehicles();
+            Values.Add(new Population() { Name = "Liczba pracowników", Count = numberOfEmployees });
+
+            Values.Add(new Population() { Name = "Liczba pojazdów", Count = numberOfVehicles });
+            Values.Add(new Population() { Name = "Używane pojazdy", Count = numberOfUsedVehicles });
+            Values.Add(new Population() { Name = "Nieużywane pojazdy", Count = numberOfUnusedVehicles });
+
+            Values.Add(new Population() { Name = "Wszystkie paczki", Count = numberOfAllPackages });
+            Values.Add(new Population() { Name = "Dostarczane paczki", Count = numberOfPackagesWithEmployee });
+            Values.Add(new Population() { Name = "Wolne paczki", Count = numberOfPackagesWithoutEmployee });
+
+
+
+        }
+    }
+}
