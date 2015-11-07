faeff19ff2377d3440933b361e7995c01d59ab6e
Marcin Chudy <marcinchudy94@gmail.com>
2015-11-08 00:25:44 +0100

    Display enum description in the data grid instead of its name

---
+++ InstantDelivery.Presentation/Converters/EnumToDescriptionConverter.cs
@@ -0,0 +1,25 @@
+﻿using InstantDelivery.Helpers;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace InstantDelivery.Converters
+{
+    public class EnumToDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return DependencyProperty.UnsetValue;
+            Enum @enum = (Enum)value;
+            return @enum.GetDescription();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+
+
+}
