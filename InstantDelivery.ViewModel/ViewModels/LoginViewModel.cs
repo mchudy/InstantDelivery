265824cce961075134efd3c4c83b91c02fe43ccf
Marcin Chudy <marcinchudy94@gmail.com>
2015-12-04 11:20:35 +0100

    Add login screen

---
+++ InstantDelivery.ViewModel/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace InstantDelivery.ViewModel.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public async void Login()
+        {
+
+        }
+    }
+}
