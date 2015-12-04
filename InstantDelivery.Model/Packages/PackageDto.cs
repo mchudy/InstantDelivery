a06f5f456d13395ee6f5da43960908e4bffb2d22
Marcin Chudy <marcinchudy94@gmail.com>
2015-12-04 10:21:36 +0100

    Fix git ignore issues

---
+++ InstantDelivery.Model/Packages/PackageDto.cs
@@ -0,0 +1,48 @@
+﻿using InstantDelivery.Common.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace InstantDelivery.Model
+{
+    public class PackageDto
+    {
+        /// <summary>
+        /// Identyfikator paczki
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Waga paczki
+        /// </summary>
+        public decimal Weight { get; set; }
+
+        /// <summary>
+        /// Wysokość paczki
+        /// </summary>
+        public double Height { get; set; }
+
+        /// <summary>
+        /// Szerokość paczki
+        /// </summary>
+        public double Width { get; set; }
+
+        /// <summary>
+        /// Długość paczki
+        /// </summary>
+        public double Length { get; set; }
+
+        /// <summary>
+        /// Koszt paczki
+        /// </summary>
+        public decimal Cost { get; set; }
+
+        /// <summary>
+        /// Status packi
+        /// </summary>
+        [JsonConverter(typeof(StringEnumConverter))]
+        public PackageStatus Status { get; set; }
+
+
+        public AddressDto ShippingAddress { get; set; } = new AddressDto();
+    }
+}
