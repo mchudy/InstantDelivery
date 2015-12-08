0b1e6d48fd212ba4e5b4c9b2fa0b7dbfa4103f7c
Marcin Chudy <marcinchudy94@gmail.com>
2015-12-08 17:42:38 +0100

    Basic geocoding sample

---
+++ InstantDelivery.Presentation/Maps/map.js
@@ -0,0 +1,23 @@
+﻿var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10
+    });
+    var geocoder = new google.maps.Geocoder();
+    geocodeAddress(geocoder, "Aleje Jerozolimskie, Warszawa", map);
+}
+
+function geocodeAddress(geocoder, address, resultsMap) {
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
