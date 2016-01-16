ae39ba3f43c19b9aa529f415da894cea7075388c
Marcin Chudy <marcinchudy94@gmail.com>
2016-01-17 00:33:59 +0100

    Fix file not checked into git

---
+++ InstantDelivery.Model/Packages/PackageEventDto.cs
@@ -0,0 +1,15 @@
+﻿using InstantDelivery.Common.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+namespace InstantDelivery.Model.Packages
+{
+    public class PackageEventDto
+    {
+        public DateTime Date { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public PackageEventType EventType { get; set; }
+    }
+}
