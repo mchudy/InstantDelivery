04833146227dac26ff49bfcd863bd05248cc3aaf
Marcin Chudy <marcinchudy94@gmail.com>
2016-01-17 20:04:24 +0100

    Clean up HTML

--- InstantDelivery.Web/App_Start/BundleConfig.cs
+++ InstantDelivery.Web/App_Start/BundleConfig.cs
@@ -4,7 +4,6 @@ namespace InstantDelivery.Web
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -20,11 +19,11 @@ namespace InstantDelivery.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js", "~/Scripts/scripts.js"));
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css", "~/Content/styles.css"));
+                      "~/Content/site.css", "~/Content/font-awesome.css"));
         }
     }
 }
