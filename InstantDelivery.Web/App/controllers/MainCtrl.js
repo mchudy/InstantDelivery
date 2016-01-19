3637bf8bb7ff1c2e764b96c9d2a8ab93cac44af1
Tygrysku <duszakp@student.mini.pw.edu.pl>
2016-01-19 17:16:37 +0100

    spa getting started

---
+++ InstantDelivery.Web/App/controllers/MainCtrl.js
@@ -0,0 +1,11 @@
+﻿angular.module('app').controller("MainController", function ($http) {
+    var vm = this;
+    vm.title = "ngtitle";
+
+    $http.get('http://instantdelivery.azurewebsites.net/api/packages/1/history/').then(
+     function (response) {
+         vm.title = response;
+     }, function (response) {
+         vm.title = response;
+     });
+});
---
+++ InstantDelivery.Web/Controllers/PortalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InstantDelivery.Web.Controllers
+{
+    public class PortalController : Controller
+    {
+        // GET: Portal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
