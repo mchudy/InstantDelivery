73760c494803fe4c4e9ed3f6016534da277a682b
Marcin Chudy <marcinchudy94@gmail.com>
2016-01-19 23:01:41 +0100

    Add Angular routing example

--- InstantDelivery.Web/App/App.js
+++ InstantDelivery.Web/App/App.js
@@ -1,1 +1,18 @@
-﻿angular.module("app", []);
+﻿angular.module("app", ['ngRoute'])
+.config(function($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'Index',
+                controller: 'MainController'
+            })
+            .when('/packages', {
+                templateUrl: 'MyPackages',
+                controller: 'MainController'
+            })
+            .when('/sendPackage', {
+                templateUrl: 'SendPackage',
+                controller: 'MainController'
+            });
+
+        $locationProvider.html5Mode(true);
+    });
--- InstantDelivery.Web/Controllers/PortalController.cs
+++ InstantDelivery.Web/Controllers/PortalController.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace InstantDelivery.Web.Controllers
 {
     public class PortalController : Controller
     {
-        // GET: Portal
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult MyPackages()
+        {
+            return PartialView();
+        }
+        
+        public ActionResult SendPackage()
+        {
+            return PartialView();
         }
     }
 }
