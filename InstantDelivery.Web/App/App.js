e23a9577232298926aec43154b565ce10cd05016
Marcin Chudy <marcinchudy94@gmail.com>
2016-01-20 22:49:04 +0100

    Fix routing

--- InstantDelivery.Web/App/App.js
+++ InstantDelivery.Web/App/App.js
@@ -1,18 +1,19 @@
 ﻿angular.module("app", ['ngRoute'])
 .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'Index',
-            controller: 'MainController'
-        })
-        .when('/packages', {
-            templateUrl: 'MyPackages',
-            controller: 'MainController'
-        })
-        .when('/sendPackage', {
-            templateUrl: 'SendPackage',
-            controller: 'MainController'
-        });
+        $routeProvider
+            .when('/', {
+                templateUrl: 'MyPackages',
+                controller: 'MainController'
+            })
+            .when('/packages', {
+                templateUrl: 'MyPackages',
+                controller: 'MainController'
+            })
+            .when('/sendPackage', {
+                templateUrl: 'SendPackage',
+                controller: 'MainController'
+            })
+            .otherwise({ redirectTo: '/' });
 
     $locationProvider.html5Mode(true);
 }]);
