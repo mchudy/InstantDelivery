6380acc76b3e9cba0cbbe22ebf8a2432dc328b51
Marcin Chudy <marcinchudy94@gmail.com>
2016-01-30 20:05:13 +0100

    Add validation for the address form

--- InstantDelivery.Web/App/controllers/SendPackageController.js
+++ InstantDelivery.Web/App/controllers/SendPackageController.js
@@ -1,7 +1,25 @@
 ﻿app.controller("SendPackageController", ['$scope', '$location', 'packagesService', function ($scope, $location, packagesService) {
-    $scope.package = {};
+    $scope.package = packagesService.currentPackage;
 
-    $scope.moveToDimensions = function() {
-        $location.path('sendPackage/dimensions');
+    $scope.moveToDimensions = function (isValid) {
+        if (isValid) {
+            packagesService.currentPackage = $scope.package;
+            $location.path('sendPackage/dimensions');
+        } else {
+            showErrors($scope.addressForm);
+        }
+    }
+
+    $scope.moveToSummary = function() {
+        packagesService.currentPackage = $scope.package;
+        $location.path('sendPackage/summary');
+    }
+
+    function showErrors(form) {
+        angular.forEach(form.$error, function (field) {
+            angular.forEach(field, function (errorField) {
+                errorField.$setTouched();
+            });
+        });
     }
 }]);
--- InstantDelivery.Web/App/services/packagesService.js
+++ InstantDelivery.Web/App/services/packagesService.js
@@ -1,5 +1,10 @@
 ﻿app.factory('packagesService', ['$http', 'config', function ($http, config) {
     return {
+        /**
+         * Used for keeping the state between sendPackage views
+         */
+        currentPackage: {},
+
         getPage: function (pageIndex, pageSize) {
             var query = "pageSize=" + pageSize + "&pageIndex=" + pageIndex;
             return $http.get(config.baseUri + 'customers/packages/page?' + query);
